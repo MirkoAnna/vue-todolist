d78e7dd0815c3fe558b2d54d18d3649a181a68ed
MirkoAnna <82913293+MirkoAnna@users.noreply.github.com>
2022-02-12 22:35:28 +0100

    Milestone 1 e 3 + Bonus

--- main.js
+++ main.js
@@ -2,7 +2,32 @@ const root = new Vue (
     {
         el: '#root',
         data: {
-
+            newToDo: '',
+            toDos: [
+                {
+                    text: 'ToDo1',
+                    done: false
+                },
+                {
+                    text: 'ToDo2',
+                    done: true
+                },
+                {
+                    text: 'ToDo3',
+                    done: true
+                },
+            ],
+        },
+        methods: {
+            addToDo() {
+                if (this.newToDo != '') {
+                    this.toDos.push({text: this.newToDo, done: false});
+                    this.newToDo = '';
+                }
+            },
+            doneUndone(toDo) {
+                toDo.done = !toDo.done;
+            },
         }
     }
-)
+);
